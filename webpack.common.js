d8a98e65092822552e324a3aa917bc6a4be7cccc
anton-zotov <anton.a.zotov@gmail.com>
2020-04-18 22:27:48 +0200

    gh-pages

--- webpack.common.js
+++ webpack.common.js
@@ -2,12 +2,7 @@ const path = require('path');
 // const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
     entry: './src/index.js',
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist',
-    },
     // plugins: [
     //     new CleanWebpackPlugin(),
     // ],
